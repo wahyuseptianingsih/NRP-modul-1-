b51af69ea07eb474c6f46f383341ddffcd9672f0
wahyuseptianingsih <102017695+wahyuseptianingsih@users.noreply.github.com>
2022-10-27 12:00:21 +0700

    Add files via upload

---
+++ GetName.java
@@ -0,0 +1,7 @@
+import java.net.*;
+public class GetName{
+public static void main(String args[]) throws Exception{ InetAddress host = null ;
+ host = InetAddress.getLocalHost();
+ System.out.println("Nama komputer Anda :"+host.getHostName());
+}
+}
